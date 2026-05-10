ae72a2aac1ab3e2996961abb7f3293d09861f8bf
nogginly <nogginly@icloud.com>
2026-05-09 23:46:00 -0400

    Fix another index error for degerate tables

--- src/termify/markdown/table_renderer.cr
+++ src/termify/markdown/table_renderer.cr
@@ -24,7 +24,7 @@ module Termify
       # Tablo doesn't handle escaped text when calculating
       # column widths and wrapping text.
       private def self.strip_escaped_codes(text)
-        text.gsub(/\e\[[0-9;]*m/, "")
+        text.gsub(/\e\[[0-9;]*m/, "") if text
       end
 
       def self.render(rows : Array(Array(String)),
@@ -40,41 +40,67 @@ module Termify
         border = Tablo::Border.new(:fancy,
           styler: ->(border_chars : String) { border_chars.colorize(:dark_gray).to_s })
 
-        # Tablo doesn't work with escape codes; so we strip it out.
-        table = Tablo::Table.new(rows[1..-1], border: border,
-          row_divider_frequency: 1,
-          header_styler: ->(content : String) { content.colorize.bold.to_s })
-        max_cols.times do |i|
-          align = convert(alignments[i]?)
-          table.add_column(i,
-            header_alignment: align,
-            body_alignment: align,
-            header: strip_escaped_codes(header_cells[i]? || "")) do |row|
-            strip_escaped_codes(row[i]) || ""
+        begin
+          # Safely try and render table
+          # Tablo doesn't work with escape codes; so we strip it out.
+          table = Tablo::Table.new(rows[1..-1], border: border,
+            row_divider_frequency: 1,
+            header_styler: ->(content : String) { content.colorize.bold.to_s })
+
+          max_cols.times do |i|
+            align = convert(alignments[i]?)
+            table.add_column(i,
+              header_alignment: align,
+              body_alignment: align,
+              header: strip_escaped_codes(header_cells[i]?) || "") do |row|
+              strip_escaped_codes(row[i]?) || ""
+            end
           end
-        end
-        # Pack to min size first so we can check the width
-        table.pack(autosize: true)
-        # If it's really wide and not indented, re-pack to terminal width
-        # Else if it's wider than 80, repack to 80
-        # Else OK
-        if table.total_table_width > 100 && indent == 0
-          Tablo::Config.terminal_capped_width = true
+          # Pack to min size first so we can check the width
           table.pack(autosize: true)
-          Tablo::Config.terminal_capped_width = false
-        elsif table.total_table_width > 80
-          table.pack(80, autosize: true)
-        end
-        # Handle indent by prefixing each line of table render
-        if indent > 0
-          prefix = " " * indent
-          table.to_s.each_line(chomp: false) do |line|
-            io << prefix << line
+          # If it's really wide and not indented, re-pack to terminal width
+          # Else if it's wider than 80, repack to 80
+          # Else OK
+          if table.total_table_width > 100 && indent == 0
+            Tablo::Config.terminal_capped_width = true
+            table.pack(autosize: true)
+            Tablo::Config.terminal_capped_width = false
+          elsif table.total_table_width > 80
+            table.pack(80, autosize: true)
           end
-        else
-          io << table
+          # Handle indent by prefixing each line of table render
+          if indent > 0
+            prefix = " " * indent
+            table.to_s.each_line(chomp: false) do |line|
+              io << prefix << line
+            end
+          else
+            io << table
+          end
+        rescue ex
+          # Tablo failed. Render table raw
+          render_raw_table(rows, io, indent)
         end
         io.puts
+      end
+
+      private def self.render_raw_table(rows : Array(Array(String)),
+                                        io : IO, indent : Int32 = 0) : Nil
+        # Tablo failed. Render table raw
+        prefix = indent > 0 ? " " * indent : ""
+        rows.each_with_index do |row, i|
+          io << prefix << row.join(" | ").strip
+          io.puts
+          if i.zero?
+            # divider line after first row
+            io << prefix
+            row.each_with_index do |col, col_ix|
+              io << " | " if col_ix > 0
+              io << " " * col.size
+            end
+            io.puts
+          end
+        end
       end
     end
   end
