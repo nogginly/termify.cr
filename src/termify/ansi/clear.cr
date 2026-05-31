149d38c42a654a994bbe60e10bba972a175c40e3
nogginly <nogginly@icloud.com>
2026-05-31 00:07:59 -0400

    Fix to use correct `Erase` enum name

--- src/termify/ansi/clear.cr
+++ src/termify/ansi/clear.cr
@@ -9,18 +9,18 @@ module Termify::ANSI
     # Clear the screen and move the cursor to the top left corner.
     def self.clear_screen(how_much = Erase::All) : String
       case how_much
-      when EraseMode::Before then "\e[1J"
-      when EraseMode::After  then "\e[0J"
-      when EraseMode::All    then "\e[H\e[2J"
+      when Erase::Before then "\e[1J"
+      when Erase::After  then "\e[0J"
+      when Erase::All    then "\e[H\e[2J"
       end
     end
 
     # Erases some or all of the line the cursor is on.
     def self.clear_line(how_much = Erase::All) : String
       case how_much
-      when EraseMode::Before then "\e[1K"
-      when EraseMode::After  then "\e[0K"
-      when EraseMode::All    then "\e[2K"
+      when Erase::Before then "\e[1K"
+      when Erase::After  then "\e[0K"
+      when Erase::All    then "\e[2K"
       end
     end
   end
